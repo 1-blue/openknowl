63de97e2663cd225b19b36dab352a4af97469c94
1-blue <1-blue98@naver.com>
2023-09-26 09:04:42 +0900

    Fix: axios timeout
    테스트 배포 환경에서 네트워크 속도가 느려서 3초 이상 걸리는 요청이 있음

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 /** 2023/09/19 - 서버로 요청할 때 사용할 인스턴스 - by 1-blue */
 const axiosInstance = axios.create({
   baseURL: process.env['NEXT_PUBLIC_END_POINT'],
-  timeout: 3000,
+  timeout: 1000 * 20,
   withCredentials: true,
 });
 
